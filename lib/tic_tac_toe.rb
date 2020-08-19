340a21327ba4232ce2b85b66b84335c14e385c34
Rebecca Congi <rncongi@gmail.com>
2020-08-19 01:23:03 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -75,13 +75,11 @@ class TicTacToe
   end   
   
   def won?
-    
     WIN_COMBINATIONS.any? do |combos|
       if position_taken?(combos[0]) && @board[combos[0]] == @board[combos[1]] && @board[combos[1]] == @board[combos[2]]
         return combos
       end 
     end   
-    
   end
   
   def full?
